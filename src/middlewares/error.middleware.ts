0bae4909a224d3f70ef26ac4d6cf65c5f6e47c2b
Demetrius Panovitch <demetrius.mp789@gmail.com>
2022-05-11 00:35:28 -0400

    fix: remove redundant middleware from authRouter, and add new error handler

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,9 +1,14 @@
 import { PrismaClientValidationError } from '@prisma/client/runtime';
 import { ErrorRequestHandler } from 'express';
+import { JsonWebTokenError } from 'jsonwebtoken';
 
 const errorMiddleware: ErrorRequestHandler = async (err, req, res, next) => {
   if (res.headersSent) {
     next(err);
+  }
+
+  if (err instanceof JsonWebTokenError) {
+    return res.status(401).json({ message: 'Invalid token' });
   }
 
   if (err instanceof PrismaClientValidationError) {
--- src/routers/auth.router.ts
+++ src/routers/auth.router.ts
@@ -1,11 +1,10 @@
 import * as controller from '$controllers/auth.controller';
-import authMiddleware from '$middlewares/auth.middleware';
 import { Router } from 'express';
 
 const authRouter = Router();
 
 authRouter.post('/sign-up', controller.handleSignUp);
 authRouter.post('/sign-in', controller.handleSignIn);
-authRouter.get('/me', authMiddleware, controller.handleMe);
+authRouter.get('/me', controller.handleMe);
 
 export default authRouter;
