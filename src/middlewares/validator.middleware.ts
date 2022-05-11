f567e426cc7458afaa278f2fc8a8d6f4b466f7bb
Demetrius Panovitch <demetrius.mp789@gmail.com>
2022-05-11 09:47:09 -0400

    fix: use status 422 on validation error

--- src/middlewares/validator.middleware.ts
+++ src/middlewares/validator.middleware.ts
@@ -7,7 +7,7 @@ export default function validationMiddleware(validations: ValidationChain[]) {
 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      return res.status(422).json({ errors: errors.array() });
     }
 
     return next();
