b5ae5b43d9aa0eb701d30f087b9f16c4dada6684
Demetrius Panovitch <demetrius.mp789@gmail.com>
2022-05-11 01:30:53 -0400

    feat: add significant validation messages

--- src/validators/auth.validator.ts
+++ src/validators/auth.validator.ts
@@ -1,25 +1,46 @@
 import prisma from '$lib/prisma';
 import { body } from 'express-validator';
 
+async function isUniqueEmail(value: string) {
+  const user = await prisma.user.findUnique({
+    where: {
+      email: value,
+    },
+  });
+
+  if (user) {
+    return Promise.reject(Error('Email already in use.'));
+  }
+
+  return Promise.resolve();
+}
+
 export const validateSignUpBody = [
-  body('name').isString().isLength({ min: 3 }),
-  body('email').isEmail().normalizeEmail().custom(async (value) => {
-    const user = await prisma.user.findUnique({
-      where: {
-        email: value,
-      },
-    });
+  body('name')
+    .isString()
+    .withMessage('Name must be a string.')
+    .isLength({ min: 3 })
+    .withMessage('Name must have at least 3 characters.'),
 
-    if (user) {
-      return Promise.reject(Error('Email already in use.'));
-    }
+  body('email')
+    .isEmail()
+    .withMessage('Invalid email.')
+    .normalizeEmail()
+    .custom(isUniqueEmail)
+    .withMessage('Email already in use.'),
 
-    return Promise.resolve();
-  }),
-  body('password').isLength({ min: 8 }),
+  body('password')
+    .isLength({ min: 8 })
+    .withMessage('Password must have at least 8 characters.'),
 ];
 
 export const validateSignInBody = [
-  body('email').isEmail().normalizeEmail(),
-  body('password').isLength({ min: 8 }),
+  body('email')
+    .isEmail()
+    .withMessage('Invalid email.')
+    .normalizeEmail(),
+
+  body('password')
+    .isLength({ min: 8 })
+    .withMessage('Password must have at least 8 characters.'),
 ];
